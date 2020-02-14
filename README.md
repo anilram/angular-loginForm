0918f285ec816252d8b863d252887d94273805b7
anilram <anilram.laxmisetti@qison.com>
2020-02-14 00:57:41 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-uqbzdn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-uqbzdn)
---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component,OnInit } from '@angular/core';
+import {Login} from './model/login';
+import accounts from './data/accounts.json';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent implements OnInit{
+  constructor(){}
+  model: Login;
+  accountTypes:any;
+  ngOnInit(){
+   this.model = new Login('e','d');
+   this.accountTypes=accounts;
+  }
+ 
+  onSubmit(name : string,pwd: string){
+    console.log(name +" "+ pwd);
+  }
+
+  click(event : any){
+    console.log(event.target.value);
+  }
+  
+}
